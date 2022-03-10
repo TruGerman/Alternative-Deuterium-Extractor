2b23a73e973745dbc1682b19eb1e98e208ab1402
TruGerman <benni007.pro@gmail.com>
2022-03-10 16:01:20 +0100

    Cached loop variables, floatified everything just in case, moved textures and settings to their own class, ran a simple code cleanup

--- AltDeuteriumExtractorMod.cs
+++ AltDeuteriumExtractorMod.cs
@@ -26,13 +26,13 @@ namespace AltDeuteriumExtractor
             ls.Label("ADE_EfficiencySetting".Translate(), -1F, "ADE_EfficiencySettingTooltip".Translate());
             ls.TextFieldNumeric(ref settings.efficiency, ref settings.bufferEfficiency, 0.0001F, float.MaxValue);
             ls.Label("ADE_WaterPerTickSetting".Translate(), -1F, "ADE_WaterPerTickSettingTooltip".Translate());
-            ls.TextFieldNumeric( ref settings.waterPerTick, ref settings.bufferWaterPerTick, 0.001F, float.MaxValue);
+            ls.TextFieldNumeric(ref settings.waterPerTick, ref settings.bufferWaterPerTick, 0.001F, float.MaxValue);
             ls.Label("ADE_PowerDrawSetting".Translate(), -1f, "ADE_PowerDrawSettingTooltip".Translate());
             ls.TextFieldNumeric(ref settings.powerDraw, ref settings.bufferPowerDraw, float.MinValue, float.MaxValue);
             ls.Label("ADE_MaxDeuteriumSetting".Translate(), -1F, "ADE_MaxDeuteriumSettingTooltip".Translate());
             ls.TextFieldNumeric(ref settings.maxDeuterium, ref settings.bufferMaxDeuterium, 1, int.MaxValue);
             ls.Label("ADE_MaxWaterSetting".Translate(), -1F, "ADE_MaxWaterSettingTooltip".Translate());
-            ls.TextFieldNumeric( ref settings.maxWater, ref settings.bufferMaxWater, 1, int.MaxValue);
+            ls.TextFieldNumeric(ref settings.maxWater, ref settings.bufferMaxWater, 1, int.MaxValue);
             ls.End();
         }
 
@@ -41,28 +41,5 @@ namespace AltDeuteriumExtractor
             base.WriteSettings();
             CompDeuteriumProcessor.inverseEfficiency = 1F / settings.efficiency;
         }
-    }
-
-    public class AltDeuteriumExtractorModSettings : ModSettings
-    {
-        public float efficiency = 0.005F, waterPerTick = 0.08333F, powerDraw = 1200F;
-        public int maxDeuterium = 50, maxWater = 500;
-        public string bufferEfficiency, bufferWaterPerTick, bufferPowerDraw, bufferMaxDeuterium, bufferMaxWater;
-
-        public override void ExposeData()
-        {
-            base.ExposeData();
-            Scribe_Values.Look(ref efficiency, "efficiency", 0.005F, true);
-            Scribe_Values.Look(ref waterPerTick, "waterPerTick", 0.08333F, true);
-            Scribe_Values.Look(ref powerDraw, "powerDraw", 1200F, true);
-            Scribe_Values.Look(ref maxDeuterium, "maxDeuterium", 50, true);
-            Scribe_Values.Look(ref maxWater, "maxWater", 500, true);
-        }
-    }
-
-    [StaticConstructorOnStartup]
-    public static class Textures
-    {
-        public static readonly Texture2D GIZMO_POWER_LEVEL = ContentFinder<Texture2D>.Get("UI/Commands/ADE_GizmoSetPowerLevel");
     }
 }
---
+++ AltDeuteriumExtractorModSettings.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+
+namespace AltDeuteriumExtractor
+{
+    public class AltDeuteriumExtractorModSettings : ModSettings
+    {
+        public float efficiency = 0.005F, waterPerTick = 0.08333F, powerDraw = 1200F;
+        public int maxDeuterium = 50, maxWater = 500;
+        public string bufferEfficiency, bufferWaterPerTick, bufferPowerDraw, bufferMaxDeuterium, bufferMaxWater;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref efficiency, "efficiency", 0.005F, true);
+            Scribe_Values.Look(ref waterPerTick, "waterPerTick", 0.08333F, true);
+            Scribe_Values.Look(ref powerDraw, "powerDraw", 1200F, true);
+            Scribe_Values.Look(ref maxDeuterium, "maxDeuterium", 50, true);
+            Scribe_Values.Look(ref maxWater, "maxWater", 500, true);
+        }
+    }
+}
