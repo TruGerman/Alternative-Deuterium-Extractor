ffee8cc473a7513443211baab7cd099345121270
TruGerman <46131538+TruGerman@users.noreply.github.com>
2022-03-17 22:48:23 +0100

    -Deleted duplicate CompProperties class, not sure why it wasn't autodeleted when I renamed it -Removed a redundant "this" qualifier

--- CompPropertiesDeuteriumProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace AltDeuteriumExtractor
-{
-    class CompProperties_DeuteriumProcessor : CompProperties
-    {
-        public CompProperties_DeuteriumProcessor() => this.compClass = typeof(CompDeuteriumProcessor);
-    }
-}
